1b7ce509e3da9bf49a86ed7bc0665111311b6f23
Ang98 <angelcayatopa98@gmail.com>
2020-09-03 19:49:24 -0500

    angel

--- src/app/starter/starter.component.ts
+++ src/app/starter/starter.component.ts
@@ -1,9 +1,21 @@
 import { Component, AfterViewInit } from '@angular/core';
+
+
 @Component({
   templateUrl: './starter.component.html'
 })
 export class StarterComponent implements AfterViewInit {
   subtitle: string;
+
+  usuario = {
+    nombre: "Angel German",
+    apellido: "Cayatopa Yauri",
+    correo: "angel.cayatopa@unmsm.edu.pe",
+    experiencia:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum distinctio unde, assumenda praesentium illo, libero, sed recusandae sequi velit aliquam numquam qui tempora blanditiis quas deleniti quibusdam? Est, veniam.",
+      
+  };
+
   constructor() {
     this.subtitle = 'This is some text within a card block.';
   }
