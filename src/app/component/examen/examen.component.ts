a6a83fd922cffeae3ca6a27a9b6c67c67807a212
Cesar kennedy rousseau arzapalo caldas <cesar.arzapalo@unmsm.edu.pe>
2020-09-04 06:02:39 -0500

    duermo

--- src/app/component/examen/examen.component.ts
+++ src/app/component/examen/examen.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ExamenModel } from '../../models/examen.model';
+import { ExamenesServices } from '../../services/examenes.service';
 
 @Component({
   selector: 'app-examen',
@@ -7,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ExamenComponent implements OnInit {
+  examen: ExamenModel;
+  @Input() id: String;
 
-  constructor() { }
+  constructor(private examenDervices: ExamenesServices) { }
 
   ngOnInit(): void {
+    this.examen = this.examenDervices.getExamen(this.id);
   }
 
 }
